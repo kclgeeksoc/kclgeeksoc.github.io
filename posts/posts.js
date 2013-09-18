fa013aba630fa4e681fa1b161800da657620c4a4
Connor Shearwood <drake@lifein2d.co.uk>
2013-09-18 20:03:23 +0100

    First commit.

---
+++ posts/posts.js
@@ -0,0 +1,16 @@
+var post_list = {
+    "posts": [
+        {
+            "title": "A Dummy Post",
+            "source": "posts/dummy.md",
+            "author": "A. Writer"
+        },
+    ],
+    "events": [
+        {
+            "name": "Pub Quiz",
+            "tagline": "The annual geeksoc pub quiz",
+            "date": "2013-09-25"
+        },
+    ]
+};
