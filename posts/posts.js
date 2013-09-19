22e4fb678a63317c0762f41ded9a19eb0ede0916
Devon Buchanan <devon@divinenephron.co.uk>
2013-09-19 16:36:51 +0100

    Added redirect structures and a home page entry.

--- posts/posts.js
+++ posts/posts.js
@@ -5,23 +5,34 @@ var site_data = {
             "source": "posts/dummy.md",
             "author": "A. Writer",
             "data": "1990-06-14"
-        },
+        }
     ],
     "pages": [
         {
+            "name": "Home",
+            "uri": "/",
+            "source": "posts/index.md"
+        },
+        {
             "name": "About",
-            "uri": "about",
+            "uri": "/about",
             "source": "posts/about.md"
         },
         {
             "name": "Events",
-            "uri": "events",
+            "uri": "/events",
             "source": "posts/events.md"
         },
         {
             "name": "Fresher's Role Play",
-            "uri": "freshers-role-play",
+            "uri": "/freshers-role-play",
             "source": "posts/freshers-role-play.md"
+        }
+    ],
+    "redirects": [
+        {
+            "from": "/forum/.*",
+            "to": "/"
         }
     ],
     "events": [
@@ -29,6 +40,6 @@ var site_data = {
             "name": "Pub Quiz",
             "tagline": "The annual geeksoc pub quiz",
             "date": "2013-09-25"
-        },
+        }
     ]
 };
