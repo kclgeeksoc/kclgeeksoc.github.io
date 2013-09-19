464dc767e24e49c0a4507ca773e99a49fc9b8afc
Devon Buchanan <devon@divinenephron.co.uk>
2013-09-19 15:38:43 +0100

    Fixed an error caused by IE7 having no window.console object.

--- js/hemaeus.js
+++ js/hemaeus.js
@@ -43,3 +43,25 @@ function get_events(number) {
         "</ul>");  // Fetch all the events, and return [number] events that are in the future
 }
 
+/**
+ * Protect window.console method calls, e.g. console is not defined on IE
+ * unless dev tools are open, and IE doesn't define console.debug
+ * From http://stackoverflow.com/a/13817235
+ */
+(function() {
+    if (!window.console) {
+        window.console = {};
+    }
+    // union of Chrome, FF, IE, and Safari console methods
+    var m = [
+        "log", "info", "warn", "error", "debug", "trace", "dir", "group",
+        "groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
+        "dirxml", "assert", "count", "markTimeline", "timeStamp", "clear"
+    ];
+    // define undefined methods as noops to prevent errors
+    for (var i = 0; i < m.length; i++) {
+        if (!window.console[m[i]]) {
+            window.console[m[i]] = function() {};
+        }    
+    } 
+})();
