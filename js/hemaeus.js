50fa6427b808d0e5967424d12d7c800b738f5c90
Connor Shearwood <connor@skimlinks.com>
2013-09-19 14:13:44 +0100

    Debugging.

--- js/hemaeus.js
+++ js/hemaeus.js
@@ -1,14 +1,18 @@
 function get_content() {
+    console.log("Loading content");
     var current_page = window.location.pathname;
     var url_structure = current_page.split("/");
     url_structure.shift();  // Remove the empty first element
     var page_to_load = "posts/404.md";
     if (current_page == "/" || current_page == "/index.html") {
+        console.log("Found index url");
         page_to_load = "/posts/index.md";
     } else if (url_structure.length == 2 && url_structure[0] == "posts") {
+        console.log("Found post url");
         var post_name = url_structure[1];
         // Get and show the post
     } else if (url_structure.length == 1) {
+        console.log("Found page url");
         for (var page in site_data["pages"]) {
             page = site_data["pages"][page];
             if (encodeURIComponent(page["name"].toLowerCase()) + ".html" == page) {
